7ee6e2941d1fdfc0141cac0b3ce9d6673d852804
hogbergelliot <hogbergelliot@gmail.com>
2017-10-19 14:24:42 +0200

    push

--- RSSfeed/DialogForm.cs
+++ RSSfeed/DialogForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logics;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,52 @@ namespace RSSfeed
 {
     public partial class DialogForm : Form
     {
-        public DialogForm()
+        Podcast podcast;
+        PodcastList podcastList;
+        private int intervalMS;
+
+
+       
+
+        public DialogForm(Podcast aPod, PodcastList aPodList)
         {
+            podcastList = aPodList;
+            podcast = aPod;
+            load();
             InitializeComponent();
+        }
+
+        public void setHeader(string name)
+        {
+            lblTitle.Text = name;
+        }
+
+        private void load()
+        {
+            CategoryList categoryList = new CategoryList();
+            XmlCommunication.loadCategory(categoryList);
+            foreach (var item in categoryList.GetCategoryList())
+            {
+                cbNewCategories.Items.Add(item);
+            }
+        }
+
+        private void btnAddPod_Click(object sender, EventArgs e)
+        {
+            podcast.Name = txtNewName.Text;
+            podcast.Url = txtNewUrl.Text;
+            podcast.Category = (string)cbNewCategories.SelectedItem;
+            podcast.Interval = intervalMS;
+            
+            XmlCommunication.SaveListData(podcastList.GetPodcastList(), "Podcasts.xml");
+
+
+        }
+
+        private void numericUpdateFrequency_ValueChanged(object sender, EventArgs e)
+        {
+            int outcome = (int)numericUpdateFrequency.Value;
+            intervalMS = TimeConverter.hourToMS(outcome);
         }
     }
 }
