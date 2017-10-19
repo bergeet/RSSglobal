f63915dffaf25bc85d09a6f98d0ef6c6e389b6ad
Martin SArling <martin@sarling.se>
2017-10-19 12:52:38 +0200

    episodesvclass

---
+++ RSSfeed/Logics/Episodes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logics
+{
+    class Episodes
+    {
+        private string episodeTitle = null;
+        private string episodeUrl = null;
+
+        public string _avsnittsTitel { get; set; }
+        public string _avsnittsUrl { get; set; }
+
+        public Episodes(string titel, string url)
+        {
+            episodeTitle = titel;
+            episodeUrl = url;
+        }
+
+        public string GetAvsnittsTitel()
+        {
+            return episodeTitle;
+        }
+        public string GetAvsnittsUrl()
+        {
+            return episodeUrl;
+        }
+    }
+}
